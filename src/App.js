ad744f573c41b0550b00047a9c389cb19af5338b
saichittala <sai.chittala@gmail.com>
2025-03-16 13:42:06 +0530

    new changes on project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './home.js';
 import Compressimages from './pages/compressimages.js';
 import ConvertTojpg from './pages/convertjpg.js';
 import ConvertTopng from './pages/convertpng.js';
 import ConvertTowebp from './pages/convertwebp.js';
 import ConvertToPdf from './pages/convertpdf.js';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 
-function App() {
+function AppContent() {
+  const location = useLocation(); // Get the current location
+
+  // Define routes where the footer should NOT be displayed
+  const noFooterRoutes = ['/convertpdf', '/convertwebp', '/convertpng', '/convertjpg', '/compressimages']; // Add paths as needed
+
+  // Check if the current route is in the noFooterRoutes array
+  const shouldShowFooter = !noFooterRoutes.includes(location.pathname);
+
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -21,13 +29,17 @@ function App() {
         <Route path="/convertpdf" element={<ConvertToPdf />} />
         <Route path="/convertwebp" element={<ConvertTowebp />} />
       </Routes>
-      <Footer />
+      {shouldShowFooter && <Footer />} {/* Conditionally render Footer */}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
-// "homepage": "https://saichittala.github.io/snapimg-test",
-
